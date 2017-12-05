7701bca417f85f77e3c4cd5ae0ade61c302bcae0
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-12-05 18:11:07 -0500

    added button for front page

--- BagOfFunnies/app/src/main/java/com/example/songt/bagoffunnies/MainMenu.java
+++ BagOfFunnies/app/src/main/java/com/example/songt/bagoffunnies/MainMenu.java
@@ -2,12 +2,23 @@ package com.example.songt.bagoffunnies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button mJokeButton,mRiddleButton,mKnockButton,mDadJokeButton,mPunButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        mJokeButton = (Button) findViewById(R.id.jokeButton);
+        mRiddleButton = (Button)findViewById(R.id.riddleButton);
+        mKnockButton = (Button) findViewById(R.id.knockButton);
+        mDadJokeButton = (Button) findViewById(R.id.dadJokeButton);
+        mPunButton = (Button) findViewById(R.id.punButton);
+
+
     }
 }
