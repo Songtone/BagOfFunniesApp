f495cc0a5531504bcb89d6bf5d7dea0601a9a176
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-12-05 21:21:02 -0500

    Added some buttons to the riddle page

--- BagOfFunnies/app/src/main/java/com/example/songt/bagoffunnies/RiddlePage.java
+++ BagOfFunnies/app/src/main/java/com/example/songt/bagoffunnies/RiddlePage.java
@@ -2,12 +2,18 @@ package com.example.songt.bagoffunnies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RiddlePage extends AppCompatActivity {
+
+    private Button mShowRiddleAnswerButton,mMainMenuButton,mNextRiddleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riddle_page);
+
+        mShowRiddleAnswerButton = (Button) findViewById(R.id.showRiddleAnswer);
+
     }
 }
